a82c77bd6c5e5a92eb175dce48616c085d015815
jvorvick <49791235+jvorvick@users.noreply.github.com>
2022-05-09 20:49:55 -0700

    angular tutorial

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -20,6 +20,10 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes'; // URL to web api
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
@@ -28,16 +32,26 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
+        tap(_ => this.log('fetched heroes')),
         catchError(this.handleError<Hero[]>('getHeroes', []))
       );
   }
 
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    // For now, assume that a hero with the specified 'id' always exists.
-    // Error handling will be added in the next step of the tutorial
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`update hero id=${hero.id}`)),
+      catchError(this.handleError<any>(`updateHero`))
+    );
   }
 
   /*
